0e9b66f767584319f619f307aff6e2cbbc6650a0
Ashton Mehta <anmehta26@utexas.edu>
2021-10-30 18:37:23 -0500

    m

--- audio.js
+++
@@ -1,7 +0,0 @@
-const nodePickle = require('node-pickle');
-
-// Convert pickled object to JSON object
-nodePickle.load(mlp_classifier.model)
-.then(data => ({
-
-}))
---
+++ input.py
@@ -0,0 +1,59 @@
+import pickle
+import soundfile # to read audio file
+import numpy as np
+import librosa # to extract speech features
+import glob
+import os
+import pickle # to save model after training
+
+def extract_feature(file_name, **kwargs):
+    """
+    Extract feature from audio file `file_name`
+        Features supported:
+            - MFCC (mfcc)
+            - Chroma (chroma)
+            - MEL Spectrogram Frequency (mel)
+            - Contrast (contrast)
+            - Tonnetz (tonnetz)
+        e.g:
+        `features = extract_feature(path, mel=True, mfcc=True)`
+    """
+    mfcc = kwargs.get("mfcc")
+    chroma = kwargs.get("chroma")
+    mel = kwargs.get("mel")
+    contrast = kwargs.get("contrast")
+    tonnetz = kwargs.get("tonnetz")
+    with soundfile.SoundFile(file_name) as sound_file:
+        X = sound_file.read(dtype="float32")
+        sample_rate = sound_file.samplerate
+        if chroma or contrast:
+            stft = np.abs(librosa.stft(X))
+        result = np.array([])
+        if mfcc:
+            mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
+            result = np.hstack((result, mfccs))
+        if chroma:
+            chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
+            result = np.hstack((result, chroma))
+        if mel:
+            mel = np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T,axis=0)
+            result = np.hstack((result, mel))
+        if contrast:
+            contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T,axis=0)
+            result = np.hstack((result, contrast))
+        if tonnetz:
+            tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X), sr=sample_rate).T,axis=0)
+            result = np.hstack((result, tonnetz))
+    return result
+
+model = pickle.load(open("result/mlp_classifier.model", "rb"))
+print("Please talk")
+filename = "test.wav"
+# record the file (start talking)
+# record_to_file(filename)
+# extract features and reshape it
+features = extract_feature(filename, mfcc=True, chroma=True, mel=True).reshape(1, -1)
+# predict
+result = model.predict(features)[0]
+# show the result !
+print("result:", result)
